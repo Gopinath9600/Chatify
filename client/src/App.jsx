cb35a26af8bbfec12dcc26b624ce2d5e6cca7d6c
Gopinath Barat <baratgnath@gmail.com>
2024-02-12 01:03:02 +0530

    Initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="p-8 text-red-500">
+          Hi it's Gopinath with a new project
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
